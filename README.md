aa92cd9b9d56da245a6f0e1793d0b03a28dbdb40
Alex Sancho <asancho@keclab.com>
2018-03-28 12:51:19 +0200

    update

---
+++ README.md
@@ -0,0 +1,44 @@
+Network Subsite Menu
+====================
+
+Posted in case anyone finds it useful.  This is definitely a scratch your own niche (get it??) kind of plugin.
+
+Basically, if you have a multisite and you want to create a global menu with some or all of your subsites and show which subsite you're on, this will help you build that menu. 
+
+To use it, Network Activate it and then go to Settings -> Network Menu.
+
+You'll see a list of your subsites, with a checkbox to enable or disable that subsite in the menu. There is also a field to set the menu label. 
+
+## Implementation
+
+Once you've configured it, just drop this somewhere:
+```php
+wp_nav_menu( array('menu' => 'network_subsite_menu') );
+```
+
+In reality, the plugin doesn't really use `wp_nav_menu`, so you're going to be pretty dissappointed if you try to pass other options.  It's building out the menu manually, using the standard WordPress nav menu HTML and then circumventing the whole nav menu function by directly returning the output.
+
+The only parameter that will work is `echo`.
+
+## Filters
+
+If you need anything beyond the basics, these filters should get you there in theory:
+
+### asnm_network_subsite_menu_settings
+
+Filter the whole settings object.  You could change the order of menu items here. 
+
+### asnm_network_subsite_menu_before
+
+Lets you insert menu items before the main menu item loop. 
+
+### asnm_network_subsite_menu_after
+
+Lets you insert menu items after the main menu item loop. 
+
+### asnm_network_subsite_menu_site_info
+
+Filter the url, label, and set whether or not the current menu item is the current one. 
+
+To see how these actually work, look at the code dawg. 
+
---
+++ network-subsite-menu.php
@@ -0,0 +1,223 @@
+<?php
+/*
+Plugin Name: Network Subsite Menu
+Description: Show a menu with the network subsites.
+Version: 1.0.0
+Author: Alex Sancho <asancho@keclab.com>
+*/
+
+namespace Asancho;
+
+/**
+ * Composer Include
+ */
+require __DIR__ . '/vendor/autoload.php';
+
+class NetworkSubsiteMenu extends \WordPress_SimpleSettings {
+
+	/**
+	 * Plugin prefix
+	 *
+	 * @var string
+	 */
+	public $prefix = '_asnm';
+
+	/**
+	 * For WordPress Simple Settings
+	 *
+	 * @var bool
+	 */
+	public $network_only = true;
+
+	/**
+	 * CGD_NetworkSubsiteMenu constructor.
+	 */
+	public function __construct() {
+		parent::__construct();
+
+		add_action( 'network_admin_menu', [ $this, 'add_menu' ] );
+		add_action( 'pre_wp_nav_menu', [ $this, 'show_menu' ], 100, 2 );
+	}
+
+	/**
+	 * Add network menu
+	 */
+	public function add_menu() {
+		add_submenu_page( 'settings.php', 'Network Menu', 'Network Menu', 'manage_options', 'network-menu', [ $this, 'show_admin' ] );
+	}
+
+	/**
+	 * Show menu
+	 *
+	 * @param string $result Result menu.
+	 * @param array  $args Menu args.
+	 *
+	 * @return string
+	 */
+	public function show_menu( $result, $args ) {
+		$blog_id = get_current_blog_id();
+
+		$menu_settings = apply_filters( 'asnm_network_subsite_menu_settings', $this->get_setting( 'site_settings' ) );
+
+		// Only run if we have the right theme location input
+		if ( $args->menu !== 'network_subsite_menu' ) {
+			return $result;
+		}
+
+		// If we don't have any subsites, return
+		if ( empty( $menu_settings['enabled_sites'] ) ) {
+			return $result;
+		}
+
+		$result = '';
+		$result .= '<ul id="network_subsite_menu" class="menu menu-network-subsite">';
+
+		$result              .= apply_filters( 'asnm_network_subsite_menu_before', '' );
+		$current_already_set = false;
+
+		$enabled_sites = $menu_settings['enabled_sites'];
+
+		if ( $this->get_setting( 'menu_order' ) !== false ) {
+			$ordered_sites = explode( ',', $this->get_setting( 'menu_order' ) );
+
+			if ( \count( $ordered_sites ) === \count( $enabled_sites ) ) {
+				$enabled_sites = $ordered_sites;
+			}
+		}
+
+		foreach ( $enabled_sites as $site_id ) {
+			$site_info = apply_filters( 'asnm_network_subsite_menu_preload_site_info', false, $site_id );
+
+			if ( ! $site_info ) {
+				$site_info = get_blog_details( $site_id, true );
+
+				if ( ! \is_object( $site_info ) ) {
+					continue;
+				}
+			}
+
+			$site_info->blogname = $menu_settings['labels'][ $site_id ] ?? $site_info->blogname;
+			$site_info->siteurl = $menu_settings['urls'][ $site_id ] ?? get_site_url( $site_id );
+
+			// Set current
+			$site_info->current = false;
+			if ( apply_filters( 'asnm_network_subsite_menu_is_current_site', $blog_id === (int) $site_id, $site_id ) ) {
+				$site_info->current = true;
+			}
+
+			// Allow for extensibility
+			$site_info = apply_filters( 'asnm_network_subsite_menu_site_info', $site_info, $site_id );
+
+			$class = '';
+			if ( $site_info->current && ! $current_already_set ) {
+				$class = 'current-menu-item';
+			}
+
+			$result .= sprintf(
+				'<li id="menu-item-%1$s" class="menu-item menu-item-type-network-subsite menu-item-%1$s %2$s"><a href="%3$s">%4$s</a></li>',
+				$site_id,
+				$class,
+				$site_info->siteurl,
+				$site_info->blogname
+			);
+
+			if ( $site_info->current ) {
+				$current_already_set = true; // only set one menu item as current
+			}
+		}
+
+		$result .= apply_filters( 'asnm_network_subsite_menu_after', '', $blog_id );
+
+		$result .= '</ul>';
+
+		return $result;
+	}
+
+	/**
+	 * Show plugin options
+	 */
+	public function show_admin() {
+		$sites = get_sites();
+		$sites = apply_filters( 'asnm_network_subsite_menu_sites', $sites );
+
+		$menu_settings = $this->get_setting( 'site_settings' );
+		$network_sites = $menu_settings['enabled_sites'] ?? [];
+		?>
+
+		<div class="wrap">
+			<h2>Network Menu Settings</h2>
+			<form method="post" action="<?php echo filter_var( 'REQUEST_URI', INPUT_SERVER ); ?>">
+				<?php $this->the_nonce(); ?>
+
+				<table class="form-table">
+					<tbody>
+					<?php
+					/* @var \WP_Site $s */
+					foreach ( $sites as $s ) :
+						?>
+						<tr>
+							<th scope="row" valign="top">
+								<label><?php echo $s->id; ?> - <?php echo $s->siteurl; ?></label>
+							</th>
+							<td>
+								<p>
+									<label>
+										<input type="checkbox"
+										       name="<?php echo $this->get_field_name( 'site_settings' ); ?>[enabled_sites][]"
+										       value="<?php echo $s->id; ?>"<?php checked( in_array( $s->id, $network_sites ), true ); ?> /> Show in menu.
+									</label>
+								</p>
+
+								<p>
+									<label>
+										<input type="text"
+										       name="<?php echo $this->get_field_name( 'site_settings' ); ?>[urls][<?php echo $s->id; ?>]; ?>"
+										       value="<?php echo isset( $menu_settings['urls'][ $s->id ] ) ? htmlspecialchars( $menu_settings['urls'][ $s->id ] ) : $s->siteurl ?>"/><br/>
+										Menu url for <?php echo $s->blogname; ?> entry.
+									</label>
+								</p>
+
+
+								<p>
+									<label>
+										<input type="text"
+										       name="<?php echo $this->get_field_name( 'site_settings' ); ?>[labels][<?php echo $s->id; ?>]; ?>"
+										       value="<?php echo isset( $menu_settings['labels'][ $s->id ] ) ? htmlspecialchars( $menu_settings['labels'][ $s->id ] ) : $s->blogname ?>"/><br/>
+										Menu label for <?php echo $s->blogname; ?> entry.
+									</label>
+								</p>
+
+								<p>
+									<label>
+										<input type="text"
+										       name="<?php echo $this->get_field_name( 'site_settings' ); ?>[mobile_labels][<?php echo $s->id; ?>]; ?>"
+										       value="<?php echo isset( $menu_settings['mobile_labels'][ $s->id ] ) ? htmlspecialchars( $menu_settings['mobile_labels'][ $s->id ] ) : $s->blogname ?>"/><br/>
+										Mobile Menu label for <?php echo $s->blogname; ?> entry. (Default: Desktop label)
+									</label>
+								</p>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+					<tr>
+						<th scope="row" valign="top">
+							<label>Menu Order</label>
+						</th>
+						<td>
+							<label>
+								<input type="text"
+								       name="<?php echo $this->get_field_name( 'menu_order' ); ?>"
+								       value="<?php echo $this->get_setting( 'menu_order' ); ?>"/><br/>
+								Menu order. Separated by commas. Must include all menu items above.
+							</label>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+				<?php submit_button(); ?>
+			</form>
+		</div>
+		<?php
+	}
+}
+
+new NetworkSubsiteMenu();
